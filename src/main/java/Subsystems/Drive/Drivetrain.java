fc24f0e5ef4a0542dabe0b84b04e2e3ac7077374
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-09-16 11:19:17 -0500

    drive funct

--- src/main/java/Subsystems/Drive/Drivetrain.java
+++ src/main/java/Subsystems/Drive/Drivetrain.java
@@ -1,13 +1,15 @@
 package Subsystems.Drive;
 
 import Subsystems.SwerveModules.SwerveModule;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.Odometry3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry3d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Drivetrain extends SubsystemBase{
+public class Drivetrain extends SubsystemBase {
     SwerveModule frontLeft;
     SwerveModule frontRight;
     SwerveModule backLeft;
@@ -15,7 +17,27 @@ public class Drivetrain extends SubsystemBase{
     SwerveDriveKinematics kinematics;
     SwerveDriveOdometry3d odometry3d;
     private final Translation2d FLpose;
-public Drivetrain(){
-    
-}    
+    private final Translation2d FRpose;
+    private final Translation2d BLpose;
+    private final Translation2d BRpose;
+    public  SwerveModule[] driveArray = new SwerveModule[]{frontLeft,frontRight,backLeft,backRight};
+
+    public Drivetrain() {
+        FLpose = new Translation2d(12.25, 12.25);
+        FRpose = new Translation2d(-12.25, 12.25);
+        BLpose = new Translation2d(12.25, -12.25);
+        BRpose = new Translation2d(-12.25, -12.25);
+        frontLeft = new SwerveModule(1, 2, 3, getName());
+        frontRight = new SwerveModule(4, 5, 6, getName());
+        backLeft = new SwerveModule(7, 8, 9, getName());
+        backRight = new SwerveModule(10, 11, 12, getName());
+    }
+// i think this is robot centric......
+    public void driveXY(double vel, double theta){
+        for(SwerveModule i : driveArray){
+            i.setState(new SwerveModuleState(vel,new Rotation2d(theta)));
+        }
+
+
+    }
 }
